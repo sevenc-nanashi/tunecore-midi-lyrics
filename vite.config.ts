1cbc5f51adf1ede2b1710c6b1fb5ff496fcd5a1b
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-12-16 10:55:59 +0900

    feat: add update url

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
       },
       userscript: {
         match: "https://www.tunecore.co.jp/member/release/*",
+        downloadURL:
+          "https://raw.githubusercontent.com/sevenc-nanashi/tunecore-midi-lyrics/built/tunecore-midi-lyrics.user.js",
+        updateURL:
+          "https://raw.githubusercontent.com/sevenc-nanashi/tunecore-midi-lyrics/built/tunecore-midi-lyrics.user.js",
       },
     }),
     {
