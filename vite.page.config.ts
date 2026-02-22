1954842d7f31b73e529e7a0617f52f7a849c2cb2
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-02-22 10:08:47 +0900

    fix: baseを修正

--- vite.page.config.ts
+++ vite.page.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [vue()],
+  base: "/tunecore-midi-lyrics",
   build: {
     outDir: "dist-page",
     emptyOutDir: true,
